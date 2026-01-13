6f5e82c86670f86f884b24d2afee5b0752883671
Will Bolls <williambolls98@gmail.com>
2026-01-12 19:07:33 -0800

    started adding test guns to shoot bullets

---
+++ Assets/Scripts/Shooting/TestGun.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class TestGun : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+                
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.digit1Key.wasPressedThisFrame)
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        print("Shoot!");
+    }
+}
